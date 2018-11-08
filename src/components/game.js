7119e7243b5ed91db597997c2485669a14a4aa7b
talexcrowell <talexcrowell@gmail.com>
2018-11-08 10:56:55 -0800

    created state in game.js and started chaining functions

--- src/components/game.js
+++ src/components/game.js
@@ -5,14 +5,54 @@ import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
+export default class Game extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            feedback: "Make Your Guess!",
+            guesses: ["2"],
+            newGuess: ''
+        };
+
+    }
+
+    // setNewGame(){
+    //     this.setState({
+    //         feedback: "Make Your Guess!",
+    //         guesses: this.guesses.filter(item => typeof item === 'number')
+    //     })
+    // }
+
+    // setFeedback(feedback){
+    //     this.setState({
+    //         feedback
+    //     })
+    // }
+    
+    setGuesses(guess){
+        console.log(this.state.guesses)
+        this.setState({
+            newGuess: guess,
+            guesses: [...this.state.guesses, this.state.newGuess]
+        })
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <GuessSection feedback="Make Your Guess!" 
+                    handleSubmit={e => this.handleSubmit(e)} 
+                    setGuess={guess => this.setGuesses(guess)} />
+                <GuessCount count={this.state.guesses.length} />
+                <GuessList guesses={[this.state.guesses]} />
+            </div>
+        );
+    }
+
 }
 
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -4,10 +4,12 @@ import './guess-form.css';
 
 export default function GuessForm(props) {
     return (
-        <form>
+        <form onSubmit={e => props.handleSubmit(e)}>
             <input type="text" name="userGuess" id="userGuess"
                 className="text" maxLength="3" autoComplete="off"
-                placeholder="Enter your Guess" required />
+                placeholder="Enter your Guess" required 
+                onChange={e => props.setGuess(e.target.value)}
+                />
             <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
         </form>
     );
